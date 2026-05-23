b179f17f5fb8a929c5e4e757c60b9677a1f33427
Langwen Huang <huanglangwen@outlook.com>
2026-05-23 15:28:08 +0800

    Free EBCC decode output buffer

--- src/codec.rs
+++ src/codec.rs
@@ -199,14 +199,23 @@ pub fn ebcc_decode_into(
 
     let Ok(decompressed_view) = ArrayView3::from_shape(decompressed_data.dim(), decompressed_slice)
     else {
+        let decompressed_len = decompressed_slice.len();
+        #[expect(unsafe_code)]
+        unsafe {
+            ebcc_sys::free_buffer(out_buffer.cast::<core::ffi::c_void>());
+        }
         return Err(EBCCError::InvalidInput(format!(
             "Decompressed data should be of shape {:?} but decompressed to {} elements",
             decompressed_data.shape(),
-            decompressed_slice.len()
+            decompressed_len
         )));
     };
 
     decompressed_data.assign(&decompressed_view);
+    #[expect(unsafe_code)]
+    unsafe {
+        ebcc_sys::free_buffer(out_buffer.cast::<core::ffi::c_void>());
+    }
 
     Ok(())
 }
